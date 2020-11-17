1462537f10820212f111042cdf28f2e040a8f187
Bruno Tostes <btfjulio@hotmail.com>
2020-11-17 09:56:02 -0300

    add routes and controller actions

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: %i(show edit update destroy)
+
+  def index 
+    @tasks = Task.all
+  end
+
+  def show 
+  end
+
+  def new 
+    @task = Task.new
+  end
+
+  def create 
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path
+    else
+      render :new
+    end
+  end
+
+  def edit 
+  end
+
+  def update 
+    if @task.save
+      redirect_to task_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy 
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(id: params[:id])
+  end
+
+  def task_params
+  end
+end
